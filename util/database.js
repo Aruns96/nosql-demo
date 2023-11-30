0201e67bd20f261e9800c9ec796453d29470680d
Aruns96 <arun509577@gmail.com>
2023-11-30 22:13:08 +0530

    mongo connect

---
+++ util/database.js
@@ -0,0 +1,29 @@
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+let _db;
+const mongoConnect = (callback)=>{
+  MongoClient.connect('mongodb+srv://arun509577:L1Ys3hexQBlKZnol@cluster0.nb6jckt.mongodb.net/?retryWrites=true&w=majority')
+  .then(client=>{
+    console.log("conncected");
+    _db = client.db();
+    callback();
+  })
+  .catch(e=>{
+    console.log(e);
+    throw e;
+  })
+}
+
+const getDb = ()=>{
+  if(_db){
+    return _db;
+  }
+  throw 'NO database found'
+}
+
+module.exports = {
+  mongoConnect,
+  getDb
+}
+  ;
