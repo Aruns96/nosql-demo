53db1396ae1c450320f63115d579df8fe6627042
Aruns96 <arun509577@gmail.com>
2023-12-04 09:45:19 +0530

    delete items in cart

--- models/user.js
+++ models/user.js
@@ -35,6 +35,13 @@ userSchema.methods.addToCart = function(product){
         this.cart = updatedCart;
        return this.save();
 }
+userSchema.methods.deleteItemFromCart = function(productId){
+  const updatedCartItems = this.cart.items.filter(item=>{
+           return item.productId.toString() !== productId.toString();
+         })
+         this.cart.items = updatedCartItems;
+         return this.save();
+}
 
 module.exports = mongoose.model('User',userSchema);
 
